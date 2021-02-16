218fc71cae0faaa7885659bb46b940ebaf4b1c6d
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2021-02-16 23:39:25 +0200

    Add plan for a test

---
+++ Tests/SSQLiteTests/IntegrationTests/BigStroyTest.swift
@@ -0,0 +1,38 @@
+//
+//  BigStroyTest.swift
+//
+//
+//  Created by Rostyslav Druzhchenko on 16.02.2021.
+//
+
+import XCTest
+
+@testable import ExtensionsFoundation
+
+class BigStroyTest: XCTestCase {
+
+    // MARK: - Init tests
+
+    func testBigStory() {
+
+        // Get an empty existed file with data base
+        // Copy it in a new place
+        // Create a table
+        // Insert several rows
+        // Select the rows
+        // Check the result
+        //
+
+        // Make the same for - update rows, delete rows, drop table
+
+        // Process a big SQL script that creates several tables, inserts data,
+        // creates triggers.
+
+
+        // Given
+        // When
+        // Then
+    }
+
+}
+
